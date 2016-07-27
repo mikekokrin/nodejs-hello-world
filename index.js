e06a9dddf714905f97b57b30030fdbadf5d82486
Pavel <picnixer@gmail.com>
2016-07-27 12:18:41 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+var os = require('os');
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 3000;
+
+app.get('/', function (req, res) {
+  res.send('<h3>Hello world from NodeJS</h3><br><b>Server listening port ' + port + ' on container ' + os.hostname() + '</b>');
+});
+
+app.listen(port, function () {
+  console.log('Express.js server started from ' + os.hostname() + ' on port ' + port + '\n');
+});
