42c360acf1c9627b898de471513ffcad8499c309
Pavel Evdokimov <picnixer@gmail.com>
2017-11-23 15:01:01 +0500

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ app.get('/', function (req, res) {
   res.send('<h3>Hello world from NodeJS</h3><br><b>Server listening port ' + port + ' on container ' + os.hostname() + '</b>');
 });
 
-app.listen(port, function () {
+app.listen(port, '0.0.0.0', function () {
   console.log('Express.js server started from ' + os.hostname() + ' on port ' + port + '\n');
 });
